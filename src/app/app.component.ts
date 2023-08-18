63412fd7f23eac2281de00dfa59b8a102d2a5b44
Matai <mat4school@gmail.com>
2023-08-18 16:50:42 -0400

    convert share to ids instead of input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class AppComponent implements OnInit {
   public input = `  `;
   public data: LeetifyUserData[] = []
+  private steamIds: string[] = [];
   private lastData = ``;
 
   constructor(public httpClient: HttpClient, private route: ActivatedRoute) {
@@ -20,10 +21,9 @@ export class AppComponent implements OnInit {
   public async ngOnInit() {
     this.route.queryParams.subscribe({
       next: (queryParams) => {
-        if (queryParams && btoa(queryParams['gameData']) !== this.lastData) {
-          this.lastData = btoa(queryParams['gameData']);
-          console.log(queryParams, atob(queryParams['gameData']))
-          this.input = atob(queryParams['gameData']) as string;
+        if (queryParams && queryParams['gameData'] !== this.lastData) {
+          this.lastData = queryParams['gameData'];
+          this.input = queryParams['gameData'] as string;
           if (this.input) {
             this.dataChanged();
 
@@ -38,21 +38,21 @@ export class AppComponent implements OnInit {
     this.data = [];
     var steamIds = this.input.match(/STEAM_1:\d+:\d+/g) || [];
 
-    var steam64Ids = steamIds.map((steamId) => {
+    this.steamIds = steamIds.map((steamId) => {
       var parts = steamId.split(":");
       var steam64Id = BigInt(parts[2]) * 2n + 76561197960265728n + BigInt(parts[1]);
       return steam64Id.toString();
     });
-    this.fetchData(steam64Ids);
+    this.fetchData();
   }
 
-  public async fetchData(steam64Ids: string[]) {
-    for (const steam64Id of steam64Ids) {
+  public async fetchData() {
+    for (const steam64Id of this.steamIds) {
       this.data.push(await firstValueFrom(this.httpClient.get<LeetifyUserData>("https://api.leetify.com/api/profile/" + steam64Id)));
     }
   }
 
   public share() {
-    navigator.clipboard.writeText(location.host + "?gameData=" + btoa(this.input));
+    navigator.clipboard.writeText(location.host + "?gameData=" + this.data.map(x => x.meta.steam64Id).join(','));
   }
 }
